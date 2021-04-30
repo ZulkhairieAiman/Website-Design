781a161f07544cee245888b6a2e4a655ad323536
HamizanA <20FTT2421@student.pb.edu.bn>
2021-04-30 15:23:18 +0800

    completed the change input values in ecards

--- app.js
+++ app.js
@@ -32,4 +32,40 @@ function ChangeMe(){
     var box1 = document.getElementById("title1");
     box1.innerHTML = title;
   }
+  var job = document.getElementById("box2").value;
+
+  if(document.getElementById("box2").value != 0){
+    var box2 = document.getElementById("job2");
+    box2.innerHTML = job;
+  }
+  var phone1 = document.getElementById("box3").value;
+
+  if(document.getElementById("box3").value != 0){
+    var box3 = document.getElementById("phoneno1");
+    box3.innerHTML = phone1;
+  }
+  var phone2 = document.getElementById("box4").value;
+
+  if(document.getElementById("box4").value != 0){
+    var box4 = document.getElementById("phoneno2");
+    box4.innerHTML = phone2;
+  }
+  var email = document.getElementById("box5").value;
+
+  if(document.getElementById("box5").value != 0){
+    var box5 = document.getElementById("email");
+    box5.innerHTML = email;
+  }
+  var url = document.getElementById("box6").value;
+
+  if(document.getElementById("box6").value != 0){
+    var box6 = document.getElementById("url");
+    box6.innerHTML = url;
+  }
+  var address = document.getElementById("box7").value;
+
+  if(document.getElementById("box7").value != 0){
+    var box7 = document.getElementById("address");
+    box7.innerHTML = address;
+  }
 }
